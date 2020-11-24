58a8c34268043e2b944e99a62791218084388f7b
CODICMAN <74947463+CODICMAN@users.noreply.github.com>
2020-11-24 23:54:56 +0530

    hello people

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main() {
+      printf("Hello, How are you? \n);
+      printf("I hope you are fine, and it's nice to see you! \n);
+      printf("Thank you for showing up here! \n);
+      
+      return 0;
+      }
